7f8b5f53a86347a44c3ce45f7671e53f6bcac03d
Adnan Khan <adnan@sharedtech.com.sa>
2025-08-21 17:52:40 +0300

    updated code

---
+++ web/modules/custom/sharjah_dd/src/Plugin/Block/FeedbackBlock.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\sharjah_dd\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\config_pages\Entity\ConfigPages;
+use Drupal\Core\File\FileUrlGeneratorInterface;
+use Drupal\Core\Url;
+use Drupal\media\Entity\Media;
+use Drupal\file\Entity\File;
+
+
+/**
+ * Provides a Front feedback block.
+ *
+ * @Block(
+ *   id = "feedback_block",
+ *   admin_label = @Translation("feedback Block"),
+ *   category = @Translation("sharjah_dd"),
+ * )
+ */
+final class FeedbackBlock extends BlockBase implements ContainerFactoryPluginInterface
+{
+
+  /**
+   * Constructs the plugin instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    private readonly ConfigFactoryInterface $configFactory,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self
+  {
+    return new self(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array
+  {
+
+    $config_page = ConfigPages::config('home_settings');
+    if (!$config_page) {
+      return [];
+    }
+    
+    // $description = $config_page->get('field_feedback_block_desc')->value ?? '';
+    // $highlight = $config_page->get('field_feedback_block_highligh')->value ?? '';
+    // $title = $config_page->get('field_feedback_block_title')->value ?? '';
+    
+    $items = [
+      'highlight' => $highlight,
+      'title' => $title,
+      'description' => $description,
+    ];
+    return [
+      '#theme' => 'feedback_block',
+      '#items' => $items,
+      '#cache' => [
+        'tags' => $config_page ? $config_page->getCacheTags() : [],
+        'contexts' => ['languages'],
+      ],
+
+    ];
+ 
+  }
+}
